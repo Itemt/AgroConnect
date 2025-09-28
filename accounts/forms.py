ab7680da7d56b656a7f811d89853af909ce5a531
Jhonnikek <jhonnierdiaz12@gmail.com>
2025-09-28 15:10:38 -0500

    update templates and form.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,18 +6,38 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('role', 'first_name', 'last_name', 'email')
+        labels = {
+            'first_name': 'Nombres',
+            'last_name': 'Apellidos',
+            'email': 'Correo Electrónico',
+            'role': 'Rol',
+        }
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+        labels = {
+            'first_name': 'Nombres',
+            'last_name': 'Apellidos',
+            'email': 'Correo Electrónico',
+        }
 
 class ProducerProfileForm(forms.ModelForm):
     class Meta:
         model = ProducerProfile
         fields = ['location', 'farm_description', 'main_crops']
+        labels = {
+            'location': 'Ubicación',
+            'farm_description': 'Descripción de la granja',
+            'main_crops': 'Cultivos principales',
+        }
 
 class BuyerProfileForm(forms.ModelForm):
     class Meta:
         model = BuyerProfile
         fields = ['company_name', 'business_type']
+        labels = {
+            'company_name': 'Nombre de la empresa',
+            'business_type': 'Tipo de negocio',
+        }
