df0791f53ac27911b0a7236ae887fbfa79ff43a9
Itemt <crisrague1@gmail.com>
2025-10-05 19:13:12 -0500

    Agregar funcionalidad para marcar todas las notificaciones como no leídas: se implementa una nueva vista y URL para gestionar esta acción, junto con los botones correspondientes en la interfaz de usuario. Se actualiza el script para manejar la lógica de la nueva funcionalidad.

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('notifications/list/', views.notifications_list, name='notifications_list'),
     path('notifications/mark-all-read/', views.notifications_mark_all_read, name='notifications_mark_all_read'),
+    path('notifications/mark-all-unread/', views.notifications_mark_all_unread, name='notifications_mark_all_unread'),
     path('notifications/', views.notifications_page, name='notifications_page'),
 ]
 
